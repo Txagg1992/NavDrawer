6bb4e1ba901a0762841d47252c0de4183bca7c74
txagg1992 <curiousaps@gmail.com>
2019-07-18 15:57:40 -0500

    Second commit: Create drawer layout, layout includes LinearLayout, FrameLayout for activity and fragments.  under LinearLayout add navigationView with header and menu items. Add drawer instance to main activity, added two string values, added drawer listener to toggle open/closed. Add transparent value to the styles.xml file.

--- NavDrawerBuild/app/src/main/java/com/curiousca/navdrawerbuild/MainActivity.java
+++ NavDrawerBuild/app/src/main/java/com/curiousca/navdrawerbuild/MainActivity.java
@@ -1,13 +1,44 @@
 package com.curiousca.navdrawerbuild;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)){
+            mDrawer.isDrawerOpen(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void initViews(){
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
