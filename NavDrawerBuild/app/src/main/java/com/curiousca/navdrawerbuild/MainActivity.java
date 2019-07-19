985930db06a8f74b8d093d9d36dcf05ddd879d8a
txagg1992 <curiousaps@gmail.com>
2019-07-18 22:35:12 -0500

    Final commit: Set up onClick interface, switch statement for menu items. Set up basic fragments. Handle opening fragment view

--- NavDrawerBuild/app/src/main/java/com/curiousca/navdrawerbuild/MainActivity.java
+++ NavDrawerBuild/app/src/main/java/com/curiousca/navdrawerbuild/MainActivity.java
@@ -1,13 +1,17 @@
 package com.curiousca.navdrawerbuild;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawer;
 
@@ -17,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FoodMenuFragment()).commit();
+        }
     }
 
     @Override
@@ -35,10 +44,39 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_menu_items:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FoodMenuFragment()).commit();
+                break;
+            case R.id.nav_order_food:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new OrderFoodFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share this App", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contact us for no apparent reason", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_chat:
+                Toast.makeText(this, "Send a text, Let's chat", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
