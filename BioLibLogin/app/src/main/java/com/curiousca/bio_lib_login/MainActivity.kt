885cfed006fdc567ffe6fa8b3b96654972becfd1
txagg1992 <curiousaps@gmail.com>
2019-11-18 12:26:25 -0600

    Initial Commit.

---
+++ BioLibLogin/app/src/main/java/com/curiousca/bio_lib_login/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.curiousca.bio_lib_login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
